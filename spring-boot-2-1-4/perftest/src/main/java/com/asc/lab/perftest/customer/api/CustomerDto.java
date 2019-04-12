23e172ce441ac3cbc72dda6490ecbcbeb4b6c5a5
wojtek.suwala@outlook.com <wojtek.suwala@outlook.com>
2019-04-12 10:22:13 +0200

    customer

---
+++ spring-boot-2-1-4/perftest/src/main/java/com/asc/lab/perftest/customer/api/CustomerDto.java
@@ -0,0 +1,19 @@
+package com.asc.lab.perftest.customer.api;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CustomerDto {
+    private String name;
+    private Boolean isVip;
+    private String country;
+    private String zipCode;
+    private String city;
+    private String street;
+}
