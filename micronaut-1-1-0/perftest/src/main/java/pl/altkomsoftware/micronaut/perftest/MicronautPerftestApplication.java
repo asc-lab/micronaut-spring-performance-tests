6d7ab5913e2ff01807e02743f045137660c8c4bd
Robert Witkowski <robert.witkowski@altkom.pl>
2019-04-15 16:02:35 +0200

    Change *App file names. Change default port in spring boot app. Remove withJansi from logback.xml config.

--- micronaut-1-1-0/perftest/src/main/java/pl/altkomsoftware/micronaut/perftest/MicronautPerftestApplication.java
+++ micronaut-1-1-0/perftest/src/main/java/pl/altkomsoftware/micronaut/perftest/MicronautPerftestApplication.java
@@ -2,9 +2,9 @@ package pl.altkomsoftware.micronaut.perftest;
 
 import io.micronaut.runtime.Micronaut;
 
-public class Application {
+public class MicronautPerftestApplication {
 
     public static void main(String[] args) {
-        Micronaut.run(Application.class);
+        Micronaut.run(MicronautPerftestApplication.class);
     }
 }
--- spring-boot-2-1-4/perftest/src/main/java/pl/altkomsoftware/spring/perftest/SpringBootPerftestApplication.java
+++ spring-boot-2-1-4/perftest/src/main/java/pl/altkomsoftware/spring/perftest/SpringBootPerftestApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PerftestApplication {
+public class SpringBootPerftestApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PerftestApplication.class, args);
+		SpringApplication.run(SpringBootPerftestApplication.class, args);
 	}
 
 }
